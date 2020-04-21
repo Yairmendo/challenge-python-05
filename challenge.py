95c10489f6fe09a0d0ffee3840c2ad15f244aca9
Yair Mendoza <yairmendo@gmail.com>
2020-04-20 21:47:24 -0500

    done but looking for improvement

--- challenge.py
+++ challenge.py
@@ -45,64 +45,57 @@ if __name__ == '__main__':
 
         def setUp(self):
             # Initialize the needed values for the tests
-            self.side = 5
-            self.base = 5
-            self.height = 2
-            self.diagonal_1 = 10 
-            self.diagonal_2 = 4
-            self.base_minor = 2
-            self.base_major = 6
-            self.perimeter = 20
-            self.apothem = 6
-            self.radius = 6
+            self.parameters = {
+                'side' = 5
+                'base' = 5
+                'height' = 2
+                'diagonal_1' = 10 
+                'diagonal_2' = 4
+                'base_minor' = 2
+                'base_major' = 6
+                'perimeter' = 20
+                'apothem' = 6
+                'radius' = 6
+                }
 
             
         def test_square_area(self):
-            square_answer = square_area(self.side)
+            square_answer = square_area(self.parameters['side'])
             self.assertEqual(square_answer, 25)
 
 
         def test_rectangle_area(self):
-            rectangle_answer = rectangle_area(self.base, self.height)
+            rectangle_answer = rectangle_area(self.parameters['base'], self.parameters['height'])
             self.assertEqual(rectangle_answer, 10)
             
 
         def test_triangle_area(self):
-            triangle_answer = triangle_area(self.base, self.height)
+            triangle_answer = triangle_area(self.parameters['base'], self.parameters['height'])
             self.assertEqual(triangle_answer, 5)
 
 
         def test_rhombus_area(self):
-            rhombus_answer = rhombus_area(self.diagonal_1, self.diagonal_2 )
+            rhombus_answer = rhombus_area(self.parameters['diagonal_1'], self.parameters['diagonal_2'])
             self.assertEqual(rhombus_answer, 20)
 
 
         def test_trapezoid_area(self):
-            trapezoid_answer = trapezoid_area(self.base_minor, self.base_major, self.height)
+            trapezoid_answer = trapezoid_area(self.parameters['base_minor'], self.parameters['base_major'], self.parameters['height'])
             self.assertEqual(trapezoid_answer, 8)
 
 
         def test_regular_polygon_area(self):
-            regular_polygon_answer = regular_polygon_area(self.perimeter, self.apothem)
+            regular_polygon_answer = regular_polygon_area(self.parameters['perimeter'], self.parameters['apothem'])
             self.assertEqual(regular_polygon_answer, 60)
 
 
         def test_circumference_area(self):
-            circumference_answer = circumference_area(self.radius)
+            circumference_answer = circumference_area(self.parameters['radius'])
             self.assertEqual(circumference_answer, 113)
 
         def tearDown(self):
         # Delete the needed values for the tests
-            del(self.side)
-            del(self.base)
-            del(self.height)
-            del(self.diagonal_1)
-            del(self.diagonal_2)
-            del(self.base_minor)
-            del(self.base_major)
-            del(self.perimeter)
-            del(self.apothem)
-            del(self.radius)
+        del(self.parameters)
 
 
     unittest.main()
