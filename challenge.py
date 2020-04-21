6245a5381ac7e8237cd210c2a1ba23d102cfff9a
Yair Mendoza <yairmendo@gmail.com>
2020-04-20 21:38:37 -0500

    done but looking for improvement

--- challenge.py
+++ challenge.py
@@ -3,52 +3,39 @@ import math
 
 def square_area(side):
     """Returns the area of a square"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return side**2
+    
 
 
 def rectangle_area(base, height):
     """Returns the area of a rectangle"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return base*height
 
 
 def triangle_area(base, height):
     """Returns the area of a triangle"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return (base*height)/2
 
 
 def rhombus_area(diagonal_1, diagonal_2):
     """Returns the area of a rhombus"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return (diagonal_1*diagonal_2)/2
 
 
 def trapezoid_area(base_minor, base_major, height):
     """Returns the area of a trapezoid"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return ((base_minor+base_major)/2)*height
 
 
 def regular_polygon_area(perimeter, apothem):
     """Returns the area of a regular polygon"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    pass
+    return (perimeter*apothem)/2
 
 
 def circumference_area(radius):
     """Returns the area of a circumference"""
-    # You have to code here
-    # REMEMBER: Tests first!!!
-    # Use math.pi for π value
-    pass
+    return round(math.pi*(radius**2))
+
 
 
 if __name__ == '__main__':
@@ -58,31 +45,68 @@ if __name__ == '__main__':
 
         def setUp(self):
             # Initialize the needed values for the tests
-            pass
+            self.side = 5
+            self.base = 5
+            self.height = 2
+            self.diagonal_1 = 10 
+            self.diagonal_2 = 4
+            self.base_minor = 2
+            self.base_major = 6
+            self.perimeter = 20
+            self.apothem = 6
+            self.radius = 6
 
+            
         def test_square_area(self):
-            # Make this test first...
+            square_answer = square_area(self.side)
+            self.assertEqual(square_answer, 25)
+
 
         def test_rectangle_area(self):
-            # Make this test first...
+            rectangle_answer = rectangle_area(self.base, self.height)
+            self.assertEqual(rectangle_answer, 10)
+            
 
         def test_triangle_area(self):
-            # Make this test first...
+            triangle_answer = triangle_area(self.base, self.height)
+            self.assertEqual(triangle_answer, 5)
+
 
         def test_rhombus_area(self):
-            # Make this test first...
+            rhombus_answer = rhombus_area(self.diagonal_1, self.diagonal_2 )
+            self.assertEqual(rhombus_answer, 20)
+
 
         def test_trapezoid_area(self):
-            # Make this test first...
+            trapezoid_answer = trapezoid_area(self.base_minor, self.base_major, self.height)
+            self.assertEqual(trapezoid_answer, 8)
+
 
         def test_regular_polygon_area(self):
-            # Make this test first...
+            regular_polygon_answer = regular_polygon_area(self.perimeter, self.apothem)
+            self.assertEqual(regular_polygon_answer, 60)
+
 
         def test_circumference_area(self):
-            # Make this test first...
+            circumference_answer = circumference_area(self.radius)
+            self.assertEqual(circumference_answer, 113)
 
         def tearDown(self):
-            # Delete the needed values for the tests
-            pass
+        # Delete the needed values for the tests
+            del(self.side)
+            del(self.base)
+            del(self.height)
+            del(self.diagonal_1)
+            del(self.diagonal_2)
+            del(self.base_minor)
+            del(self.base_major)
+            del(self.perimeter)
+            del(self.apothem)
+            del(self.radius)
+
 
     unittest.main()
+
+
+
+
